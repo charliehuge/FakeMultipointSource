432880e047c4d13ba6d2714f4f5d59626e20ad87
Charlie Huguenard <charlie.huguenard@gmail.com>
2017-05-31 19:24:55 -0600

    initial commit

---
+++ Assets/Scripts/FollowBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBounds : MonoBehaviour 
+{
+	public Transform ListenerTransform;
+	public Collider BoundsCollider;
+
+	private void Update()
+	{
+		if (null == BoundsCollider || null == ListenerTransform)
+		{
+			return;
+		}
+
+		transform.position = BoundsCollider.ClosestPoint(ListenerTransform.position);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawWireSphere(transform.position, 0.5f);
+	}
+}
